892fa5fb206e6402a707b9a971ac4e470b66ae5f
Ha young, Kim <jeremiah91@naver.com>
2021-04-23 15:27:08 +0900

    getting time information from Web API

--- clock.js
+++ clock.js
@@ -1,5 +1,22 @@
-function init () {
-    
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector(".js-title");
+
+// 현재 시간에서 시계를 불러올 함수 :
+function getTime() {
+  //시간정보 가져오기
+  const date = new Date();
+  const seconds = date.getSeconds();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+
+  // 시간정보 입력
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${
+    minutes < 10 ? `0${minutes}` : minutes
+  } : ${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
 init();
