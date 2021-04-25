0da9217e50e64de98dcf8f990091ad3f40e444f0
Ha young, Kim <jeremiah91@naver.com>
2021-04-25 23:48:16 +0900

    saving user name

--- greeting.js
+++ greeting.js
@@ -5,6 +5,24 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  // form 에서 값을 입력하고 엔터를 누르면 발생하는 event 를 멈춤
+  event.preventDefault();
+  // input 에 입력한 사용자 이름을 가져옴
+  const currentValue = input.value;
+  // 가져온 이름을 h4에 보여줌
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,9 +32,10 @@ function paintGreeting(text) {
 
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
-  // user 가 없는 경우
   console.log(currentUser);
+  // user 가 없는 경우
   if (currentUser === null) {
+    askForName();
     // user 가 있는 경우
   } else {
     paintGreeting(currentUser);
