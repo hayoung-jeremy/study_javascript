03ff5ec4bb1a5ba51d36a431d046eeb68d577923
Ha young, Kim <jeremiah91@naver.com>
2021-04-29 09:07:49 +0900

    fetching weather API

--- weather.js
+++ weather.js
@@ -1,11 +1,33 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "4202a96e6da9b590dc2f1b421ceb72f6";
 const COORDS = "coords";
+
+// javaScript는 새로고침없이도 백그라운드에서 계속적으로 데이터를 가져올 수 있다:
+// 날씨 가져올 함수
+function getWeather(lat, lng) {
+  // fetch 로 data를 받고, then: 이전 함수가 끝난 후 실행할 함수
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log(json);
+      // 온도 :
+      const temperature = json.main.temp;
+      // 위치 :
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 // 좌표 저장
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
-// 좌표 가져오기를 성공했을때 실행할 함수
+// 좌표 가져오기를 성공했을때 실행할 함수 (위치정보를 가져오고, 그에 해당하는 날씨정보를 가져온다.)
 function handleGeoSuccess(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
@@ -17,6 +39,9 @@ function handleGeoSuccess(position) {
   };
   // 좌표를 저장할 함수에 가져온 좌표를 arg 로 전달
   saveCoords(coordsObj);
+
+  // 해당 좌표에대한 날씨정보를 가져옴
+  getWeather(latitude, longitude);
 }
 
 // 좌표를 가져오지 못했을때 실행할 함수
@@ -31,10 +56,14 @@ function askForCoords() {
 
 function loadCoords() {
   const loadedCoords = localStorage.getItem(COORDS);
+  // 좌표 정보가 없다면, 유저에게 좌표 정보를 요청
   if (loadedCoords === null) {
     askForCoords();
-  } else {
-    // get weather
+  }
+  // 좌표 정보가 이미 있을 경우, 해당 좌표의 날씨정보를 가져옴
+  else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 
